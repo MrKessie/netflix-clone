2b3a898f3f455fff9d025576cfb10b3fb79e1455
MrKessie <kessiekoranteng29@gmail.com>
2023-07-08 11:38:20 +0000

    My Second Commit

--- js/main.js
+++ js/main.js
@@ -24,6 +24,34 @@ function onLeave(){
     img.style.width = "300px"
 }
 
+function loadData(){
+    const div = document.createElement("div");
+    const lbl = document.createElement("label");
+    const lbl2 = document.createElement("label");
+    const p = document.createElement("p");
+    const p2 = document.createElement("p");
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+      .then((json) => {
+        p.innerText= json["title"];
+        lbl.innerText = "Title of work";
+        lbl2.innerText = json["completed"];
+        p2.innerText = "Completed of work"
+        div.appendChild(lbl);
+        div.appendChild(p);
+        div.appendChild(p2);
+        div.appendChild(lbl2)
+        section.appendChild(div);
+        
+        
+
+
+        
+      })
+}
+loadData();
+
 btn.addEventListener("click", printValue);
 img.addEventListener("mouseover", onHover);
 img.addEventListener("mouseout", onLeave);
